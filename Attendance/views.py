2551c8f243cb8097da444fdfaf46dbfb34b76f3e
Ankit Rishi Gupta <36430212+WizDemirider@users.noreply.github.com>
2019-01-22 06:30:14 +0530

    Update views.py

--- Attendance/views.py
+++ Attendance/views.py
@@ -5,7 +5,7 @@ from django.views.generic import CreateView, TemplateView
 from django.contrib.auth import logout
 
 class SignUp(CreateView):
-    form_class = forms.UserCreateForm()
+    form_class = forms.UserCreateForm
     success_url = reverse_lazy('login')
     template_name = 'Attendance/signup.html'
 
