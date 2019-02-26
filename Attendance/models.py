d495a908040ac93194d31c0c3c30e5127118e719
Ankit Gupta <argankit@gmail.com>
2019-02-26 19:41:42 +0530

    database population

--- Attendance/models.py
+++ Attendance/models.py
@@ -28,7 +28,7 @@ class Teacher(models.Model):
     user = models.OneToOneField(AppUser, on_delete=models.CASCADE, primary_key=True)
     specialization = models.CharField(max_length=50, null=True, blank=True)
     teacherID = models.CharField(max_length=20)
-    subject = models.ManyToManyField(Subject, related_name='subject', through='SubjectTeacher')
+    subject = models.ManyToManyField(Subject, related_name='teacher', through='SubjectTeacher')
 
     def __str__(self):
         return self.user.getfullname()
@@ -39,7 +39,8 @@ class Div(models.Model):
     year = models.PositiveSmallIntegerField()
     division = models.CharField(max_length=10)
     subject = models.ManyToManyField(Subject, related_name='division', through="DivisionSubject")
-    classteacher = models.OneToOneField(Teacher, on_delete=models.SET_NULL, null=True, related_name='Div')
+    classteacher = models.ForeignKey(Teacher, on_delete=models.SET_NULL, null=True)
+    teacher = models.ManyToManyField(Teacher, related_name='division', through="DivisionTeacher")
 
     def __str__(self):
         yearname = ""
@@ -98,3 +99,8 @@ class DivisionSubject(models.Model):
 class StudentDivision(models.Model):
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
     division = models.ForeignKey(Div, on_delete=models.CASCADE)
+
+
+class DivisionTeacher(models.Model):
+    division = models.ForeignKey(Div, on_delete=models.CASCADE)
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
